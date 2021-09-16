2b978fa09e18290d9c2be75188fe8e70c08e590a
paigeman <poizijiang1120@foxmail.com>
2021-09-16 23:06:07 +0800

    #commit consul-payment8006模块

---
+++ consul-payment8006/src/main/java/org/fade/springclouddemo/ConsulPaymentApplication.java
@@ -0,0 +1,15 @@
+package org.fade.springclouddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConsulPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulPaymentApplication.class, args);
+    }
+
+}
---
+++ consul-payment8006/src/main/java/org/fade/springclouddemo/controller/PaymentController.java
@@ -0,0 +1,24 @@
+package org.fade.springclouddemo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.fade.springclouddemo.entity.CommonResult;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/payment")
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/consul")
+    public CommonResult<String> paymentConsul() {
+        return new CommonResult<>(200,
+                "SpringCloud run with Consul on the port " + serverPort);
+    }
+
+}
