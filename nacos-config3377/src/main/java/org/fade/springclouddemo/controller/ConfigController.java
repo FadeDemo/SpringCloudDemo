a64389a5fc048266aeceba81beed5b092d45eea9
paigeman <53284808+paigeman@users.noreply.github.com>
2022-08-03 09:06:12 +0800

    nacos配置加载及刷新断点
    nacos配置加载及刷新断点

--- nacos-config3377/src/main/java/org/fade/springclouddemo/controller/ConfigController.java
+++ nacos-config3377/src/main/java/org/fade/springclouddemo/controller/ConfigController.java
@@ -13,9 +13,17 @@ public class ConfigController {
     @Value("${config.info}")
     private String configInfo;
 
+    @Value("${share}")
+    private String shareConfig;
+
     @GetMapping("/config/info")
     public CommonResult<String> configInfo() {
         return new CommonResult<>(200, configInfo);
+    }
+
+    @GetMapping("/config/shareConfig")
+    public CommonResult<String> getShareConfig() {
+        return new CommonResult<>(200, shareConfig);
     }
 
 }
